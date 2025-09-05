7f7d0b0eca44f57d7e8804687a23f10c9c00b67e
sabique <sabiqueislam7@gmail.com>
2025-09-05 12:55:34 +0530

    fix: I did smthg

--- src/components/protected/Assistant.tsx
+++ src/components/protected/Assistant.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Assistant() {
   return (
     <div>
-      Assistant
+      Assistant.
     </div>
   )
 }
--- src/components/protected/Explorer.tsx
+++ src/components/protected/Explorer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Explorer() {
   return (
     <div>
-      Explorer
+      Explorer.
     </div>
   )
 }
